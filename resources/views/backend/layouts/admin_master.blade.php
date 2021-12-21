a0634b414d804c8dad5c1b1072bc0c8cad3d6d9b
kawsar <kawsar@gmail.com>
2021-12-21 21:12:07 +0800

    auth using

---
+++ resources/views/backend/layouts/admin_master.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+			@include('backend.partials.header')
+	
+	<body class="no-skin">		
+
+			@include('backend.partials.navbar')
+
+				<div class="main-container ace-save-state" id="main-container">
+			
+			@include('backend.partials.sidebar')
+
+
+
+			@yield('content')
+
+			@include('backend.partials.footer')
+
+			
+	</body>
+
+<!-- Mirrored from ace.jeka.by/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Dec 2021 09:41:02 GMT -->
+</html>
